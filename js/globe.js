b9c6de09a31bf881904d80cd68652c0f3038202a
sghenzy <benedetta.bigliardo@gmail.com>
2025-02-05 18:19:06 +0100

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -231,7 +231,7 @@ function animate() {
   // Mantieni il controllo della rotazione del globo e dei gruppi orbitali
   if (controls.autoRotate) {
     orbitGroups.forEach(group => {
-      group.rotation.y += 1; // Ruota i gruppi orbitali solo se l'auto-rotate è attivo
+      group.rotation.y += 5; // Ruota i gruppi orbitali solo se l'auto-rotate è attivo
     });
   }
 

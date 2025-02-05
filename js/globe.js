648b66ab3b72489321e614def47585078261457a
sghenzy <benedetta.bigliardo@gmail.com>
2025-02-05 17:16:27 +0100

    Update globe.js

--- js/globe.js
+++ js/globe.js
@@ -57,7 +57,7 @@ function init() {
   controls.maxDistance = 2.5;
   controls.enablePan = false;
   controls.autoRotate = true;
-  controls.autoRotateSpeed = 0.09;
+  controls.autoRotateSpeed = 0.1;
   controls.enableDamping = true;
   controls.dampingFactor = 0.1;
 

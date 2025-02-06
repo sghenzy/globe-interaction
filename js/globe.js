58c98d28c446641f31bb82573bed184da21742bb
sghenzy <benedetta.bigliardo@gmail.com>
2025-02-06 16:49:56 +0100

    pin color

--- js/globe.js
+++ js/globe.js
@@ -177,7 +177,7 @@ function addPins() {
 
 function createPin(labelText) {
   const pinGeometry = new THREE.SphereGeometry(0.009, 16, 16); // Ridotto da 0.015 a 0.01
-  const pinMaterial = new THREE.MeshStandardMaterial({ color: 'rgb(255, 0, 0)' }); // Rosso
+  const pinMaterial = new THREE.MeshStandardMaterial({ color: '#410000' }); // Rosso
   const pin = new THREE.Mesh(pinGeometry, pinMaterial);
 
   return pin;

d954475def5696486784c91351f8e1cf920f9e14
sghenzy <benedetta.bigliardo@gmail.com>
2025-02-06 16:26:52 +0100

    size 2

--- js/globe.js
+++ js/globe.js
@@ -139,7 +139,7 @@ function addPins() {
 
 
 function createPin(labelText) {
-  const pinGeometry = new THREE.SphereGeometry(0.01, 16, 16); // Ridotto da 0.015 a 0.01
+  const pinGeometry = new THREE.SphereGeometry(0.009, 16, 16); // Ridotto da 0.015 a 0.01
   const pinMaterial = new THREE.MeshStandardMaterial({ color: 'rgb(255, 0, 0)' }); // Rosso
   const pin = new THREE.Mesh(pinGeometry, pinMaterial);
 
